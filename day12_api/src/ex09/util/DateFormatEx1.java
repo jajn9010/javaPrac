a7febe211869f38c36831c400f420d46a85564ca
Jaejin Lee <jaejin1112@gmail.com>
2023-04-13 11:12:37 +0900

    Stop study API

--- day12_api/src/ex09/util/DateFormatEx1.java
+++ day12_api/src/ex09/util/DateFormatEx1.java
@@ -6,10 +6,12 @@ class DateFormatEx1{
 	public static void main(String[] args) {
 		Date today = new Date();
 		System.out.println(today);  //
-		SimpleDateFormat sdf1, sdf4;
-		sdf1 = new SimpleDateFormat("yy-MM-dd");
-		sdf4 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss a E");
+		SimpleDateFormat sdf1, sdf3, sdf4;
+		sdf1 = new SimpleDateFormat("yy:MM:dd");
+		sdf3 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss a E");
+		sdf4 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss a EEEE");
 		System.out.println(sdf1.format(today));	// format(Date d)
+		System.out.println(sdf3.format(today));
 		System.out.println(sdf4.format(today));
 	}
 }
--- day12_api/src/ex09/util/DateFormatEx2.java
+++ day12_api/src/ex09/util/DateFormatEx2.java
@@ -4,17 +4,17 @@ import java.text.*;
 class DateFormatEx2{
 	public static void main(String[] args) {
 		Calendar cal = Calendar.getInstance();
-		cal.set(2005, 9, 3);	
+		cal.set(1990, 10, 12);	
 		Date day = cal.getTime();
 		SimpleDateFormat sdf1, sdf2, sdf3, sdf4, sdf22;
 		sdf1 = new SimpleDateFormat("yyyy-MM-dd");
 		sdf2 = new SimpleDateFormat("yy-MM-dd E요일");
-		sdf22 = new SimpleDateFormat("yy-MM-dd e");  //?? 영문요일 
+//		sdf22 = new SimpleDateFormat("yy-MM-dd e");  //?? 영문요일 
 		sdf3 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss SSS"); // ??
 		sdf4 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss a");
 		System.out.println(sdf1.format(day)); // format(Date d)
 		System.out.println(sdf2.format(day));
-		System.out.println(sdf22.format(day));  //
+//		System.out.println(sdf22.format(day));  //
 		System.out.println(sdf3.format(day));
 		System.out.println(sdf4.format(day));
 
