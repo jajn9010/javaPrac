341e402b2419ccafa79eec9f8f789ee6317e96ba
Jaejin Lee <jaejin1112@gmail.com>
2023-08-29 10:06:52 +0900

    check http.sslverify in git bash

--- algorithm_programmers/src/Platinum/D7687.java
+++ algorithm_programmers/src/Platinum/D7687.java
@@ -10,44 +10,48 @@ public class D7687 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-		
-		while(true) {
+
+		while (true) {
 			st = new StringTokenizer(br.readLine());
-			
+
 			int lx = Integer.parseInt(st.nextToken());
 			int ly = Integer.parseInt(st.nextToken());
 			int lz = Integer.parseInt(st.nextToken());
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
 			int z = Integer.parseInt(st.nextToken());
-			
-			if((lx | ly | lz | x | y | z) == 0) break;
-			
+
+			if ((lx | ly | lz | x | y | z) == 0)
+				break;
+
 			int ans = Integer.MAX_VALUE;
-			
-			 if(x == 0 || y == 0 || z == 0) sb.append(x * x + y * y + z * z).append('\n');
-	            else {
-	                if (lx == x) {
-	                    ans = Math.min(ans, (x + y) * (x + y) + z * z);
-	                    ans = Math.min(ans, (x + z) * (x + z) + y * y);
-	                    ans = Math.min(ans, (lz + y) * (lz + y) + (lx + lz - z) * (lx + lz - z));
-	                    ans = Math.min(ans, (ly + z) * (ly + z) + (lx + ly - y) * (lx + ly - y));
-	                }
-	                if (ly == y) {
-	                    ans = Math.min(ans, (x + y) * (x + y) + z * z);
-	                    ans = Math.min(ans, (y + z) * (y + z) + x * x);
-	                    ans = Math.min(ans, (lz + x) * (lz + x) + (ly + lz - z) * (ly + lz - z));
-	                    ans = Math.min(ans, (lx + z) * (lx + z) + (lx + ly - x) * (lx + ly - x));
-	                }
-	                if (lz == z) {
-	                    ans = Math.min(ans, (z + y) * (z + y) + x * x);
-	                    ans = Math.min(ans, (x + z) * (x + z) + y * y);
-	                    ans = Math.min(ans, (lx + y) * (lx + y) + (lx + lz - x) * (lx + lz - x));
-	                    ans = Math.min(ans, (ly + x) * (ly + x) + (lz + ly - y) * (lz + ly - y));
-	                }
-	                sb.append(ans).append('\n');
-	            }
+
+			if (x == 0 || y == 0 || z == 0)
+				sb.append(x * x + y * y + z * z).append('\n');
+			else {
+				if (lx == x) {
+					ans = Math.min(ans, (x + y) * (x + y) + z * z);
+					ans = Math.min(ans, (x + z) * (x + z) + y * y);
+					ans = Math.min(ans, (lz + y) * (lz + y) + (lx + lz - z) * (lx + lz - z));
+					ans = Math.min(ans, (ly + z) * (ly + z) + (lx + ly - y) * (lx + ly - y));
+				}
+				if (ly == y) {
+					ans = Math.min(ans, (x + y) * (x + y) + z * z);
+					ans = Math.min(ans, (y + z) * (y + z) + x * x);
+					ans = Math.min(ans, (lz + x) * (lz + x) + (ly + lz - z) * (ly + lz - z));
+					ans = Math.min(ans, (lx + z) * (lx + z) + (lx + ly - x) * (lx + ly - x));
+				}
+				if (lz == z) {
+					ans = Math.min(ans, (z + y) * (z + y) + x * x);
+					ans = Math.min(ans, (x + z) * (x + z) + y * y);
+					ans = Math.min(ans, (lx + y) * (lx + y) + (lx + lz - x) * (lx + lz - x));
+					ans = Math.min(ans, (ly + x) * (ly + x) + (lz + ly - y) * (lz + ly - y));
+				}
+				
+				sb.append(ans).append('\n');
+			}
 		}
+		
 		System.out.println(sb);
 	}
 }
